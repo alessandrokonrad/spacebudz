f0386c22dec6158cc556d1a611cd0b64604c49d9
Alessandro Konrad <akonrad6@gmail.com>
2020-10-20 16:37:39 +0200

    fixed bug with grid view

--- src/views/Browse.jsx
+++ src/views/Browse.jsx
@@ -9,6 +9,7 @@ import { FloatingButton } from "../components/Button";
 const array = [...Array(300).keys()];
 
 const Browse = (props) => {
+  const [isGridFirst, setIsGridFirst] = React.useState(false);
   const [showGrid, setShowGrid] = React.useState(true);
   const [idArray, setIdArray] = React.useState(null);
   const shuffle = (array) => {
@@ -19,11 +20,21 @@ const Browse = (props) => {
   };
 
   React.useEffect(() => {
+    let isSpaceBudView = false;
     let url = window.location.href;
     if (url.includes("spacebud")) {
       setShowGrid(false);
+      isSpaceBudView = false;
       window.scrollTo(0, 0);
-    } else setShowGrid(true);
+    } else {
+      setShowGrid(true);
+      isSpaceBudView = true;
+    }
+
+    if (isGridFirst || isSpaceBudView) {
+      window.scrollTo(0, 0);
+      setIsGridFirst(true);
+    }
   }, [window.location.href]);
 
   React.useEffect(() => {
@@ -43,69 +54,71 @@ const Browse = (props) => {
       }}
     >
       {showGrid && <FloatingButton onClick={() => window.scrollTo(0, 0)} />}
-      <div
-        style={{
-          opacity: !showGrid && 0,
-          width: "100%",
-          maxWidth: 1400,
-        }}
-      >
-        <Headroom>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div style={{ marginBottom: 20, marginTop: 20 }}>
-              <Search
-                onChange={(e) => {
-                  window.scrollTo(0, 0);
-                  if (e.target.value) e.persist();
-                  if (e.target.value == "") {
-                    setIdArray(null);
-                    setTimeout(() => setIdArray(array));
-                    return;
-                  }
-                  setIdArray(null);
-                  setTimeout(() => setIdArray([e.target.value]));
-                }}
-              />
-            </div>
-          </div>
-        </Headroom>
-        <div>
-          <Spacer y={0.8} />
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <div>
-              <b style={{ fontSize: 16 }}>Total SpaceBudz:</b> 1000
-            </div>
-            <Spacer x={1} />
-            <div>
-              <Checkbox>
-                <b style={{ fontSize: 16 }}>On Sale</b>
-              </Checkbox>
-            </div>
-          </div>
-        </div>
-        <Spacer y={3} />
+      {isGridFirst && (
         <div
           style={{
-            position: !showGrid && "fixed",
-            marginBottom: 100,
+            opacity: !showGrid && 0,
+            width: "100%",
+            maxWidth: 1400,
           }}
         >
-          {idArray && <InfiniteGrid array={idArray} />}
+          <Headroom>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div style={{ marginBottom: 20, marginTop: 20 }}>
+                <Search
+                  onChange={(e) => {
+                    window.scrollTo(0, 0);
+                    if (e.target.value) e.persist();
+                    if (e.target.value == "") {
+                      setIdArray(null);
+                      setTimeout(() => setIdArray(array));
+                      return;
+                    }
+                    setIdArray(null);
+                    setTimeout(() => setIdArray([e.target.value]));
+                  }}
+                />
+              </div>
+            </div>
+          </Headroom>
+          <div>
+            <Spacer y={0.8} />
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <div>
+                <b style={{ fontSize: 16 }}>Total SpaceBudz:</b> 1000
+              </div>
+              <Spacer x={1} />
+              <div>
+                <Checkbox>
+                  <b style={{ fontSize: 16 }}>On Sale</b>
+                </Checkbox>
+              </div>
+            </div>
+          </div>
+          <Spacer y={3} />
+          <div
+            style={{
+              position: !showGrid && "fixed",
+              marginBottom: 100,
+            }}
+          >
+            {idArray && <InfiniteGrid array={idArray} />}
+          </div>
         </div>
-      </div>
+      )}
       {!showGrid && (
         <div
           style={{
